79ed91e4fa366a7c97aa0fd25c4382fa6eed86a0
Murtaaz Khan <murtaazkhan@gmail.com>
2023-11-18 15:35:42 +0400

    Updated TokenMetaData model

--- SwissKnife.Libs.Common/OAuth/Models/TokenMetaData.cs
+++ SwissKnife.Libs.Common/OAuth/Models/TokenMetaData.cs
@@ -2,6 +2,7 @@
 
 public class TokenMetaData
 {
+    public string Name { get; set; }
     public string UserName { get; set; }
     public string UserId { get; set; }
     public string UserEmailId { get; set; }
