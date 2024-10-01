e11037f1c315513b8c0b69785abf3bb3b667c6e1
Murtaaz Khan <murtaazkhan@gmail.com>
2024-10-01 15:21:36 +0400

    Updated Jwt Helper

--- SwissKnife.Libs.Common/Helpers/JwtHelper.cs
+++ SwissKnife.Libs.Common/Helpers/JwtHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace SwissKnife.Libs.Common.Helpers;
 
@@ -27,6 +29,22 @@ public static class JwtHelper
         }
 
         return result;
+    }
+
+    public static JwtSecurityToken ValidateJwtToken(string token, string signingKey)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(signingKey);
+        tokenHandler.ValidateToken(token, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        }, out SecurityToken validatedToken);
+
+        return (JwtSecurityToken)validatedToken;
     }
 }
 
